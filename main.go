0767ba2324740105304fcf616e8e94f7deb7e4cf
Michael Desa <mjdesa@gmail.com>
2016-06-27 14:03:21 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(string(body))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
